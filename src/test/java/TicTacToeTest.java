ef58d3107a9b9cce77b86e42455016683b2037c8
David Heath <heathd@users.noreply.github.com>
2017-11-30 17:56:37 +0000

    TicTacToe Kata starting point

---
+++ src/test/java/TicTacToeTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class TicTacToeTest {
+    @Test
+    public void shouldDoAThing() {
+        assertThat("black", is("white"));
+    }
+}
